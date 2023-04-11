0b3588706609ef2856cc129b669593496373bbdb
petrkathrine <129877925+petrkathrine@users.noreply.github.com>
2023-04-11 16:51:13 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import ttest_ind
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 140576679 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
@@ -11,4 +11,14 @@ def solution(x_success: int,
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+
+    x  = x_success/x_cnt
+    y = y_success/y_cnt
+
+    p = ttest_ind(x, y, equal_var=False, alternative="less").pvalue
+    
+    if p < 0.02:
+      return True
+    else: 
+      return False
+
